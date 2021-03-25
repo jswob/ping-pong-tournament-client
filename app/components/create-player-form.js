46f16a12c4b6ca27d040b23dea52f29a49984371
jswob <jacswob@gmail.com>
2021-03-25 19:35:56 +0100

    Refactor create-player-form using tasks

--- app/components/create-player-form.js
+++ app/components/create-player-form.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { dropTask } from 'ember-concurrency';
 
 export default class CreatePlayerFormComponent extends Component {
   @service store;
@@ -9,25 +10,21 @@ export default class CreatePlayerFormComponent extends Component {
   @tracked
   nickname = '';
 
-  @action
-  async createPlayer(event) {
+  @dropTask
+  *createPlayer(event) {
     event.preventDefault();
 
     const { nickname, args } = this;
 
-    try {
-      if (!nickname.length) return;
+    if (!nickname.length) return;
 
-      this._refreshForm(args);
+    this._refreshForm(args);
 
-      const createdPlayer = this.store.createRecord('player', {
-        nickname: nickname,
-      });
+    const createdPlayer = this.store.createRecord('player', {
+      nickname: nickname,
+    });
 
-      await createdPlayer.save();
-    } catch (error) {
-      throw error;
-    }
+    yield createdPlayer.save();
   }
 
   @action
@@ -35,7 +32,7 @@ export default class CreatePlayerFormComponent extends Component {
     this.nickname = event.target.value;
   }
 
-  _refreshForm = ({switchMode}) => {
+  _refreshForm = ({ switchMode }) => {
     this.nickname = '';
     switchMode();
   };
