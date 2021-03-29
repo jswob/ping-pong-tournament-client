80ee919a9e9af02fb679b9cbf5e0d3267dbb6cb7
jswob <jacswob@gmail.com>
2021-03-29 08:56:05 +0200

    Refactor game validations

--- app/validations/game.js
+++ app/validations/game.js
@@ -1,9 +1,21 @@
-import { validateNumber } from 'ember-changeset-validations/validators';
+import {
+  validateNumber,
+  validatePresence,
+} from 'ember-changeset-validations/validators';
 import validateModelType from '../validators/model-type';
+import validateUniqueness from '../validators/uniqueness';
 
 export default {
-  player1: validateModelType({ type: 'player' }),
-  player2: validateModelType({ type: 'player' }),
+  player1: [
+    validatePresence(true),
+    validateModelType({ type: 'player' }),
+    validateUniqueness({ on: 'player2' }),
+  ],
+  player2: [
+    validatePresence(true),
+    validateModelType({ type: 'player' }),
+    validateUniqueness({ on: 'player1' }),
+  ],
   sets: validateNumber({ integer: true, gt: 0 }),
   pointsToWin: validateNumber({ integer: true, gt: 0 }),
 };
