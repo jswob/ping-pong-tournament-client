dd25989a6b1a446e9d2056d848e25c8b42af8ecc
jswob <jacswob@gmail.com>
2021-03-29 11:39:51 +0200

    Create tests for game-settings component

---
+++ tests/integration/components/setup-game-form/game-settings-test.js
@@ -0,0 +1,106 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, fillIn } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module(
+  'Integration | Component | setup-game-form/game-settings',
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    hooks.beforeEach(async function () {
+      const testData = {
+        class: 'hello',
+        changeset: {
+          errors: [],
+          sets: null,
+          pointsToWin: null,
+        },
+        update: () => {},
+      };
+
+      this.setProperties(testData);
+
+      await render(hbs`
+        <SetupGameForm::GameSettings 
+          class={{this.class}}
+          @update={{this.update}} 
+          @changeset={{this.changeset}} 
+        />`);
+    });
+
+    test('it renders all data', async function (assert) {
+      assert.expect(9);
+
+      assert
+        .dom('[data-test-game-settings]')
+        .exists()
+        .hasClass('game-settings')
+        .hasClass(this.class, 'It has passed class');
+
+      assert.dom('[data-test-sets-option]').exists();
+      assert
+        .dom('[data-test-sets-option] > [data-test-header]')
+        .hasText('Number of sets');
+      assert.dom('[data-test-sets-option] [data-test-sets-input]').exists();
+
+      assert.dom('[data-test-points-option]').exists();
+      assert
+        .dom('[data-test-points-option] > [data-test-header]')
+        .hasText('Number of points to win a set');
+      assert.dom('[data-test-points-option] [data-test-points-input]').exists();
+    });
+
+    test('it correctly handles validation errors', async function (assert) {
+      assert.expect(6);
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+
+      this.set('changeset', { errors: [{ key: 'sets', validation: '' }] });
+
+      assert
+        .dom('[data-test-sets-option] [data-test-validation-error]')
+        .exists({ count: 1 });
+      assert
+        .dom('[data-test-points-option] [data-test-validation-error]')
+        .doesNotExist();
+
+      this.set('changeset', {
+        errors: [{ key: 'pointsToWin', validation: '' }],
+      });
+
+      assert
+        .dom('[data-test-sets-option] [data-test-validation-error]')
+        .doesNotExist();
+      assert
+        .dom('[data-test-points-option] [data-test-validation-error]')
+        .exists({ count: 1 });
+
+      this.set('changeset', {
+        errors: [],
+      });
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+    });
+
+    test('it correctly executes passed update action', async function (assert) {
+      assert.expect(6);
+
+      this.set('update', (changeset, key, event) => {
+        assert.ok(changeset);
+        assert.equal(key, 'sets');
+        assert.ok(event);
+      });
+
+      await fillIn('[data-test-sets-input]', 12);
+
+      this.set('update', (changeset, key, event) => {
+        assert.ok(changeset);
+        assert.equal(key, 'pointsToWin');
+        assert.ok(event);
+      });
+
+      await fillIn('[data-test-points-input]', 12);
+    });
+  }
+);
