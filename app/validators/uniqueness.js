fe890c32a58af232a1108d9709bb8e2c1316e07f
jswob <jacswob@gmail.com>
2021-03-29 08:55:39 +0200

    Create uniqueness validator

---
+++ app/validators/uniqueness.js
@@ -0,0 +1,13 @@
+export default function validateUniqueness({ on }) {
+  return (key, newValue, _oldValue, changes) => {
+    const errorMessage = `${capitalize(key)} can't be the same as ${capitalize(on)}`;
+
+    if (changes[on] && newValue.id === changes[on].id) return errorMessage;
+
+    return true;
+  };
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
