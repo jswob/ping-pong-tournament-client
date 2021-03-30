98ac7373487a7370e5fda5f4c57e6cb6655cc917
jswob <jacswob@gmail.com>
2021-03-30 16:45:14 +0200

    Fix bug in uniqueness validator

--- app/validators/uniqueness.js
+++ app/validators/uniqueness.js
@@ -2,6 +2,8 @@ export default function validateUniqueness({ on }) {
   return (key, newValue, _oldValue, changes) => {
     const errorMessage = `${capitalize(key)} can't be the same as ${capitalize(on)}`;
 
+    if (!newValue) return true;
+
     if (changes[on] && newValue.id === changes[on].id) return errorMessage;
 
     return true;
