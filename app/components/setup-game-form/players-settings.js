0b2adc283a5d9b11379459cbafcbee8c2ee73f7a
jswob <jacswob@gmail.com>
2021-03-31 13:58:02 +0200

    Remove dropTask from players-settings

--- app/components/setup-game-form/players-settings.js
+++ app/components/setup-game-form/players-settings.js
@@ -1,12 +1,10 @@
 import Component from '@glimmer/component';
-import { dropTask } from 'ember-concurrency';
 
 export default class SetupGameFormPlayersSettingsComponent extends Component {
-  @dropTask
-  *updatePlayer(changeset, key, callback, event) {
+  updatePlayer(changeset, key, callback, event) {
     const fieldToValidate = (key === 'player1') ? 'player2' : 'player1';
 
-    yield callback(changeset, key, event);
+    callback(changeset, key, event);
 
     const isOtherFieldChanged = changeset.changes.find(
       (change) => change.key === fieldToValidate
