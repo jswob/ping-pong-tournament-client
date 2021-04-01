e4733cfaefc13e86b22b153d2e781f05d651f494
jswob <jacswob@gmail.com>
2021-04-01 19:03:32 +0200

    Refactor validation tests in create-casual-game

--- tests/integration/components/create-casual-game-test.js
+++ tests/integration/components/create-casual-game-test.js
@@ -56,81 +56,6 @@ module('Integration | Component | create-casual-game', function (hooks) {
     assert.dom('[data-test-game-settings]').exists();
   });
 
-  test('it validates game correctly', async function (assert) {
-    assert.expect(16);
-
-    await render(hbs`
-      <CreateCasualGame 
-        @players={{this.players}} 
-        @callback={{this.callback}} 
-      />`);
-
-    assert.dom('[data-test-validation-error]').doesNotExist();
-
-    await click('[data-test-player1-option] .ember-basic-dropdown-trigger');
-    await click('ul > li:first-child');
-
-    assert.dom('[data-test-validation-error]').doesNotExist();
-
-    await click('[data-test-player2-option] .ember-basic-dropdown-trigger');
-    await click('ul > li:first-child');
-
-    assert.dom('[data-test-validation-error]').exists({ count: 2 });
-
-    assert
-      .dom('[data-test-player1-option] [data-test-validation-error]')
-      .exists({ count: 1 });
-    assert
-      .dom('[data-test-player2-option] [data-test-validation-error]')
-      .exists({ count: 1 });
-
-    await click('[data-test-player2-option] .ember-basic-dropdown-trigger');
-    await click('ul > li:last-child');
-
-    assert.dom('[data-test-validation-error]').doesNotExist();
-
-    await click('[data-test-player1-option] .ember-basic-dropdown-trigger');
-    await click('ul > li:last-child');
-
-    assert.dom('[data-test-validation-error]').exists({ count: 2 });
-
-    assert
-      .dom('[data-test-player1-option] [data-test-validation-error]')
-      .exists({ count: 1 });
-    assert
-      .dom('[data-test-player2-option] [data-test-validation-error]')
-      .exists({ count: 1 });
-
-    await click('[data-test-player1-option] .ember-basic-dropdown-trigger');
-    await click('ul > li:first-child');
-
-    assert.dom('[data-test-validation-error]').doesNotExist();
-
-    await fillIn('[data-test-sets-input]', -1);
-
-    assert.dom('[data-test-validation-error]').exists({ count: 1 });
-
-    assert
-      .dom('[data-test-sets-option] [data-test-validation-error]')
-      .exists({ count: 1 });
-
-    await fillIn('[data-test-sets-input]', 1);
-
-    assert.dom('[data-test-validation-error]').doesNotExist();
-
-    await fillIn('[data-test-points-input]', -1);
-
-    assert.dom('[data-test-validation-error]').exists({ count: 1 });
-
-    assert
-      .dom('[data-test-points-option] [data-test-validation-error]')
-      .exists({ count: 1 });
-
-    await fillIn('[data-test-points-input]', 1);
-
-    assert.dom('[data-test-validation-error]').doesNotExist();
-  });
-
   test('if there are errors, submit button is disabled', async function (assert) {
     assert.expect(2);
 
@@ -140,11 +65,11 @@ module('Integration | Component | create-casual-game', function (hooks) {
         @callback={{this.callback}} 
       />`);
 
-      await fillIn('[data-test-sets-input]', -1);
+    await fillIn('[data-test-sets-input]', -1);
 
-      assert.dom('[data-test-validation-error]').exists();
+    assert.dom('[data-test-validation-error]').exists();
 
-      assert.dom('[data-test-submit-button]').isDisabled();
+    assert.dom('[data-test-submit-button]').isDisabled();
   });
 
   test('if there are no errors, invokes callback with game data as argument', async function (assert) {
@@ -193,3 +118,150 @@ module('Integration | Component | create-casual-game', function (hooks) {
     await click('[data-test-submit-button]');
   });
 });
+
+module(
+  'Integration | Component | create-casual-game validation',
+  function (hooks) {
+    setupRenderingTest(hooks);
+    setupMirage(hooks);
+
+    hooks.beforeEach(async function () {
+      this.server.createList('player', 2);
+
+      const store = this.owner.lookup('service:store');
+
+      const testData = {
+        callback: () => {},
+        players: await store.findAll('player'),
+      };
+
+      this.setProperties({ ...testData });
+
+      await render(hbs`
+      <CreateCasualGame 
+        @players={{this.players}} 
+        @callback={{this.callback}} 
+      />`);
+    });
+
+    test('Both players must be selected', async function (assert) {
+      assert.expect(10);
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+
+      await click('[data-test-submit-button]');
+
+      assert.dom('[data-test-validation-error]').exists({ count: 4 });
+
+      assert
+        .dom(
+          '[data-test-player1-option] [data-test-validation-error]:first-child'
+        )
+        .exists()
+        .hasText("Player1 can't be blank");
+
+      assert
+        .dom(
+          '[data-test-player1-option] [data-test-validation-error]:last-child'
+        )
+        .exists()
+        .hasText('Player1 must have correct type');
+
+      assert
+        .dom(
+          '[data-test-player2-option] [data-test-validation-error]:first-child'
+        )
+        .exists()
+        .hasText("Player2 can't be blank");
+
+      assert
+        .dom(
+          '[data-test-player2-option] [data-test-validation-error]:last-child'
+        )
+        .exists()
+        .hasText('Player2 must have correct type');
+    });
+
+    test("Selected players can't be the same", async function (assert) {
+      assert.expect(7);
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+
+      await click('[data-test-player1-option] .ember-basic-dropdown-trigger');
+      await click('ul > li:first-child');
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+
+      await click('[data-test-player2-option] .ember-basic-dropdown-trigger');
+      await click('ul > li:first-child');
+
+      assert.dom('[data-test-validation-error]').exists({ count: 2 });
+
+      assert
+        .dom('[data-test-player1-option] [data-test-validation-error]')
+        .exists({ count: 1 })
+        .hasText("Player1 can't be the same as Player2");
+
+      assert
+        .dom('[data-test-player2-option] [data-test-validation-error]')
+        .exists({ count: 1 })
+        .hasText("Player2 can't be the same as Player1");
+    });
+
+    test('sets must be inteager greater than 0', async function (assert) {
+      assert.expect(8);
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+
+      await fillIn('[data-test-sets-input]', 'blabla');
+
+      assert.dom('[data-test-validation-error]').exists({ count: 1 });
+
+      assert
+        .dom('[data-test-sets-option] [data-test-validation-error]')
+        .exists()
+        .hasText('Sets must be a number');
+
+      await fillIn('[data-test-sets-input]', 0);
+
+      assert.dom('[data-test-validation-error]').exists({ count: 1 });
+
+      assert
+        .dom('[data-test-sets-option] [data-test-validation-error]')
+        .exists()
+        .hasText('Sets must be greater than 0');
+
+      await fillIn('[data-test-sets-input]', 1);
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+    });
+
+    test('pointsToWin number must be inteager greater than 0', async function (assert) {
+      assert.expect(8);
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+
+      await fillIn('[data-test-points-input]', 'blabla');
+
+      assert.dom('[data-test-validation-error]').exists({ count: 1 });
+
+      assert
+        .dom('[data-test-points-option] [data-test-validation-error]')
+        .exists()
+        .hasText('Points to win must be a number');
+
+      await fillIn('[data-test-points-input]', 0);
+
+      assert.dom('[data-test-validation-error]').exists({ count: 1 });
+
+      assert
+        .dom('[data-test-points-option] [data-test-validation-error]')
+        .exists()
+        .hasText('Points to win must be greater than 0');
+
+      await fillIn('[data-test-points-input]', 1);
+
+      assert.dom('[data-test-validation-error]').doesNotExist();
+    });
+  }
+);
