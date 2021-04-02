9e4ab759d6468146c0c2edd4298ab6b9025d32cb
jswob <jacswob@gmail.com>
2021-04-02 20:28:58 +0200

    Refactor create-player-form with changeset helper

--- app/components/create-player-form.js
+++ app/components/create-player-form.js
@@ -1,35 +1,23 @@
 import Component from '@glimmer/component';
-import { action, computed } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { dropTask } from 'ember-concurrency';
 import PlayerValidations from 'ping-pong-tournament-client/validations/player';
-import Changeset from 'ember-changeset';
-import lookupValidator from 'ember-changeset-validations';
 
 export default class CreatePlayerFormComponent extends Component {
+  PlayerValidations = PlayerValidations;
+  
   @service store;
 
   @tracked
   player = { nickname: null };
 
-  @computed('player')
-  get changeset() {
-    const changeset = new Changeset(
-      this.player,
-      lookupValidator(PlayerValidations)
-    );
-
-    changeset.set('nickname', '');
-
-    return changeset;
-  }
-
   @dropTask
-  *createPlayer(event) {
+  *createPlayer(changeset, event) {
     event.preventDefault();
 
-    const { changeset } = this;
+    changeset.validate();
 
     if (!changeset.isValid) {
       return;
@@ -45,8 +33,8 @@ export default class CreatePlayerFormComponent extends Component {
   }
 
   @action
-  setNickname(event) {
-    this.changeset.set('nickname', event.target.value);
+  setNickname(changeset, event) {
+    changeset.set('nickname', event.target.value);
   }
 
   refreshPlayerModel() {
