1fcbe20d9ec9a48c6f7400e537acdef674fbaaad
jswob <jacswob@gmail.com>
2021-04-02 12:30:30 +0200

    Add acceptance test for form in casual/new

--- tests/acceptance/casual/new-test.js
+++ tests/acceptance/casual/new-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -15,16 +15,71 @@ module('Acceptance | casual/new', function (hooks) {
     assert.equal(currentURL(), '/casual/new');
   });
 
-  test('/casual/new route presents itself correctly', async function (assert) {
-    assert.expect(3);
+  test('User can create a new casual game', async function (assert) {
+    assert.expect(6);
+
+    const testData = {
+      player1: 'player1',
+      player2: 'player2',
+      sets: 4,
+      pointsToWin: 27,
+    };
+
+    this.server.createList('player', 2);
+
+    let games;
+
+    const store = this.owner.lookup('service:store');
 
     await visit('/casual/new');
 
-    assert.dom('[data-test-casual-new-layout]').exists();
-    assert
-      .dom('[data-test-casual-new-layout] > [data-test-main-header]')
-      .hasText('New Game');
+    games = await store.peekAll('game');
 
-    assert.dom('[data-test-create-casual-game]').exists();
+    assert.equal(
+      games.length,
+      0,
+      'In the beginnig there is no game record in store'
+    );
+
+    await click('[data-test-player1-option] .ember-basic-dropdown-trigger');
+    await fillIn('.ember-basic-dropdown-content input', testData.player1);
+    await click('ul > li');
+
+    await click('[data-test-player2-option] .ember-basic-dropdown-trigger');
+    await fillIn('.ember-basic-dropdown-content input', testData.player2);
+    await click('ul > li');
+
+    await fillIn('[data-test-sets-input]', testData.sets);
+
+    await fillIn('[data-test-points-input]', testData.pointsToWin);
+
+    await click('[data-test-submit-button]');
+
+    games = await store.peekAll('game');
+
+    assert.equal(
+      games.length,
+      1,
+      'After submiting form game record has been created'
+    );
+
+    const game = games.firstObject;
+
+    assert.equal(
+      game.player1.content.nickname,
+      testData.player1,
+      'player1 is correct'
+    );
+    assert.equal(
+      game.player2.content.nickname,
+      testData.player2,
+      'player2 is correct'
+    );
+    assert.equal(game.sets, testData.sets, 'sets is correct');
+    assert.equal(
+      game.pointsToWin,
+      testData.pointsToWin,
+      'pointsToWin is correct'
+    );
   });
 });
