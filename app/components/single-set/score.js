c09d0a20968471373023d65cf062b6c8c1dc29e2
jswob <jacswob@gmail.com>
2021-04-08 09:29:29 +0200

    Create single-set/score component

---
+++ app/components/single-set/score.js
@@ -0,0 +1,32 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class SingleSetScoreComponent extends Component {
+  get statusClass() {
+    const { set, scoreParam, isEnabled } = this.args;
+
+    if (isEnabled) {
+      return '';
+    }
+
+    const playerIndex = +scoreParam['player'.length] - 1;
+
+    return set.winnerIndex == playerIndex ? 'winner' : 'loser';
+  }
+
+  @action
+  increment(set, scoreParam, callback) {
+    set.set(scoreParam, set.get(scoreParam) + 1);
+
+    callback(set);
+  }
+
+  @action
+  decrement(set, scoreParam, callback) {
+    if (set.get(scoreParam) > 0) {
+      set.set(scoreParam, set.get(scoreParam) - 1);
+
+      callback(set);
+    }
+  }
+}
