74ede456ddfbe7b820fd626da610a7a753b663ec
jswob <jacswob@gmail.com>
2021-04-08 09:47:23 +0200

    Create increment helper

---
+++ tests/integration/helpers/increment-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | increment', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it increments passed value', async function(assert) {
+    this.set('inputValue', 0);
+
+    await render(hbs`{{increment inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), '1');
+  });
+});
