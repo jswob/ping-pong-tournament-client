2a7aa5906c17cf14f5852c8732c4050d0c334428
jswob <jacswob@gmail.com>
2021-04-08 12:11:25 +0200

    Create single-set component

---
+++ app/components/single-set.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class SingleSetComponent extends Component {
+  get isEnabled() {
+    const { set } = this.args;
+
+    if (set.winnerIndex != undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  @action
+  settleSet(set, callback) {
+    const winnerIndex = set.settleWinner();
+
+    if (winnerIndex != undefined) {
+      set.set('winnerIndex', winnerIndex);
+
+      callback();
+    }
+  }
+}
