f093522fd95b1c26972fdd361198be29f91a84f2
jswob <jacswob@gmail.com>
2021-04-14 11:59:24 +0200

    Create value-with-icon component

---
+++ tests/integration/components/value-with-icon-test.js
@@ -0,0 +1,46 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | value-with-icon', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders itself correctly', async function (assert) {
+    assert.expect(9);
+
+    const testData = {
+      className: 'some-class',
+      value: 'some123',
+      icon: 'crown',
+    };
+
+    this.setProperties(testData);
+
+    await render(hbs`
+      <ValueWithIcon 
+        class={{this.className}} 
+        @value={{this.value}} 
+        @icon={{this.icon}} 
+      />`);
+
+    assert
+      .dom('[data-test-value-with-icon]')
+      .exists()
+      .hasClass('value-with-icon-wrapper')
+      .hasClass(testData.className, 'It has passed class');
+
+    assert
+      .dom('[data-test-value-with-icon] > [data-test-value]')
+      .exists()
+      .hasClass('value')
+      .hasText(testData.value);
+
+    assert
+      .dom('[data-test-value-with-icon] > [data-test-icon]')
+      .exists()
+      .hasClass('icon')
+
+    assert.dom('[data-test-icon] > svg').hasClass(`fa-${testData.icon}`);
+  });
+});
