486f83ce17582b17fd6baa007e0f5aaedd2d0f6c
jswob <jacswob@gmail.com>
2021-04-16 09:26:27 +0200

    Create acceptance tests for ranking route

---
+++ tests/acceptance/players/ranking-test.js
@@ -0,0 +1,99 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | players/ranking', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function () {
+    const players = this.server.createList('player', 5);
+
+    this.server.create('game', {
+      players: [players[0], players[1]],
+      winner: players[1],
+    });
+    this.server.create('game', {
+      players: [players[2], players[1]],
+      winner: players[1],
+    });
+    this.server.create('game', {
+      players: [players[3], players[4]],
+      winner: players[4],
+    });
+  });
+
+  test('It is possible to visit /players/ranking', async function (assert) {
+    assert.expect(1);
+
+    await visit('/players/ranking');
+
+    assert.equal(currentURL(), '/players/ranking');
+  });
+
+  test('/players/ranking renders itself correctly', async function (assert) {
+    assert.expect(6);
+
+    await visit('/players/ranking');
+
+    assert.dom('[data-test-page-layout]').exists();
+
+    assert.dom('[data-test-main-header]').hasText('Ranking');
+
+    assert.dom('[data-test-players-ranking]').exists();
+
+    assert.dom('[data-test-filter-input]').exists().hasValue('');
+
+    assert.dom('[data-test-single-player]').exists({ count: 5 });
+  });
+
+  test('Players are ordered correctly', async function (assert) {
+    assert.expect(5);
+
+    await visit('/players/ranking');
+
+    assert.dom('[data-test-single-player]:first-child').includesText('player2');
+
+    assert
+      .dom('[data-test-single-player]:nth-child(2)')
+      .includesText('player5');
+
+    assert
+      .dom('[data-test-single-player]:nth-child(3)')
+      .includesText('player1');
+
+    assert
+      .dom('[data-test-single-player]:nth-child(4)')
+      .includesText('player3');
+
+    assert
+      .dom('[data-test-single-player]:nth-child(5)')
+      .includesText('player4');
+  });
+
+  test('It is possible to filter players list', async function (assert) {
+    assert.expect(2);
+
+    await visit('/players/ranking');
+
+    const inputValue = 'player1';
+
+    await fillIn('[data-test-filter-input]', inputValue);
+
+    assert
+      .dom('[data-test-single-player]')
+      .exists({ count: 1 })
+      .includesText(inputValue);
+  });
+
+  test('It is possible to visit statistic page for clicked user', async function (assert) {
+    assert.expect(1);
+
+    await visit('/players/ranking');
+
+    await click('[data-test-single-player]:first-child');
+
+    assert.equal(currentURL(), '/players/2/statistics');
+  });
+});
