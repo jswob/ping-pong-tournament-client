fffd83f6d5f254946974a279ce6cc74c9e0b4c68
jswob <jacswob@gmail.com>
2021-04-16 14:04:18 +0200

    Update value-with-icon with reverse property

--- tests/integration/components/value-with-icon-test.js
+++ tests/integration/components/value-with-icon-test.js
@@ -43,4 +43,29 @@ module('Integration | Component | value-with-icon', function (hooks) {
 
     assert.dom('[data-test-icon] > svg').hasClass(`fa-${testData.icon}`);
   });
+
+  test('it correctly sets flex-row class', async function (assert) {
+    assert.expect(2);
+
+    const testData = {
+      value: 'some123',
+      icon: 'crown',
+      reverse: false,
+    };
+
+    this.setProperties(testData);
+
+    await render(hbs`
+      <ValueWithIcon  
+        @value={{this.value}} 
+        @icon={{this.icon}} 
+        @reverse={{this.reverse}}
+      />`);
+
+    assert.dom('[data-test-value-with-icon]').hasClass('flex-row');
+
+    this.set('reverse', true);
+
+    assert.dom('[data-test-value-with-icon]').hasClass('flex-row-reverse');
+  });
 });
