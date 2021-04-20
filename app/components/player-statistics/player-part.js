d65cd69b67bc9ed01ebb6eefcc47428f7cb601d4
jswob <jacswob@gmail.com>
2021-04-20 20:57:41 +0200

    Refactor player-part with range class

--- app/components/player-statistics/player-part.js
+++ app/components/player-statistics/player-part.js
@@ -1,23 +1,62 @@
 import Component from '@glimmer/component';
+import { computed } from '@ember/object';
 
 export default class PlayerStatisticsPlayerPartComponent extends Component {
+  @computed('args.player.gamesPlayed.length')
   get gamesPlayed() {
+    console.log(this.args.player.gamesPlayed.length);
+
     const { gamesPlayed } = this.args.player;
 
     return this.checkLength(gamesPlayed);
   }
 
+  @computed('args.player.gamesWon.length')
   get gamesWon() {
     const { gamesWon } = this.args.player;
 
     return this.checkLength(gamesWon);
   }
 
-  checkLength(array) {
-      if (!Array.isArray(array)) {
-        return 0;
-      }
+  @computed('gamesPlayed', 'gamesWon')
+  get winRatio() {
+    const { gamesPlayed, gamesWon } = this;
+    let result = 0;
 
-      return array.length;
+    if (!Number(gamesPlayed)) {
+      return result;
+    }
+
+    result = Math.round((gamesWon / gamesPlayed) * 100);
+
+    return result;
+  }
+
+  @computed('winRatio')
+  get rangeClass() {
+    const winRatio = this.winRatio;
+
+    let rangeStart = Math.floor((winRatio / 10)) * 10;
+    let rangeEnd = Math.ceil(winRatio / 10) * 10 - 1;
+
+    if (rangeStart > 90) {
+      rangeStart = 90;
+    }
+
+    if (rangeEnd > 89) {
+      rangeEnd = 100;
+    } else if (rangeEnd < 9) {
+      rangeEnd = 9;
+    }
+
+    return `range-${rangeStart}-${rangeEnd}`;
+  }
+
+  checkLength(object) {
+    if (!object || typeof object === 'string') {
+      return 0;
+    }
+
+    return object.length;
   }
 }
--- tests/integration/components/player-statistics/player-part-test.js
+++ tests/integration/components/player-statistics/player-part-test.js
@@ -45,27 +45,76 @@ module(
         .dom('[data-test-statistics-table] tr:nth-child(3)')
         .hasText(`Win ratio: 33%`);
     });
-    test('it correctly handles empty arrays', async function (assert) {
-      assert.expect(900);
 
-      this.set('player.gamesPlayed', "kebab");
+    test('it correctly handles empty arrays', async function (assert) {
+      assert.expect(3);
+
+      this.set('player.gamesPlayed', 'kebab');
       this.set('player.gamesWon', []);
 
       await render(
         hbs`<PlayerStatistics::PlayerPart @player={{this.player}} />`
       );
 
-      assert
-        .dom('[data-test-statistics-table] tr:nth-child(1)')
-        .hasText(`Games Played: 0`);
+      assert.dom('[data-test-games-played]').hasText(`Games Played: 0`);
+
+      assert.dom('[data-test-games-won]').hasText(`Games Won: 0`);
+
+      assert.dom('[data-test-win-ratio]').hasText(`Win ratio: 0%`);
+    });
+
+    test('it sets correct win ratio', async function (assert) {
+      assert.expect(3);
+
+      const string = 'a';
+
+      this.set('player.gamesPlayed', string.repeat(100).split(''));
+      this.set('player.gamesWon', string.repeat(0).split(''));
+
+      await render(
+        hbs`<PlayerStatistics::PlayerPart @player={{this.player}} />`
+      );
+
+      assert.dom('[data-test-win-ratio]').hasText(`Win ratio: 0%`);
+
+      this.set('player.gamesWon', string.repeat(50).split(''));
+
+      assert.dom('[data-test-win-ratio]').hasText(`Win ratio: 50%`);
+
+      this.set('player.gamesWon', string.repeat(100).split(''));
+
+      assert.dom('[data-test-win-ratio]').hasText(`Win ratio: 100%`);
+    });
+
+    test('it sets correct range class on winRatio', async function (assert) {
+      assert.expect(4);
+
+      const string = 'a';
+
+      this.set('player.gamesPlayed', string.repeat(100).split(''));
+      this.set('player.gamesWon', string.repeat(23).split(''));
+
+      await render(
+        hbs`<PlayerStatistics::PlayerPart @player={{this.player}} />`
+      );
 
       assert
-        .dom('[data-test-statistics-table] tr:nth-child(2)')
-        .hasText(`Games Won: 0`);
+        .dom('[data-test-win-ratio] > td:last-child')
+        .hasClass('range-20-29');
+
+      this.set('player.gamesWon', string.repeat(0).split(''));
+
+      assert.dom('[data-test-win-ratio] > td:last-child').hasClass('range-0-9');
+
+      this.set('player.gamesWon', string.repeat(100).split(''));
 
       assert
-        .dom('[data-test-statistics-table] tr:nth-child(3)')
-        .hasText(`Win ratio: 0%`);
+        .dom('[data-test-win-ratio] > td:last-child')
+        .hasClass('range-90-100');
+
+      this.set('player.gamesWon', string.repeat(9).split(''));
+
+      assert.dom('[data-test-win-ratio] > td:last-child').hasClass('range-0-9');
     });
   }
 );
